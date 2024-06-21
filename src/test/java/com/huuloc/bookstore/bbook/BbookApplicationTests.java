3fe11d247b852fcda3efebe4509fb6a1ace1d22a
Nguyen Huu Loc <locn562836@gmail.com>
2024-06-21 10:05:32 +0700

    first commit

---
+++ src/test/java/com/huuloc/bookstore/bbook/BbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huuloc.bookstore.bbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
