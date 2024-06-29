6df3c0ccfdfb71a93092cda963106795db5ac2be
Nguyen Huu Loc <locn562836@gmail.com>
2024-06-29 18:24:57 +0700

    set up online payment

---
+++ src/main/java/com/huuloc/bookstore/bbook/config/PayOSConfig.java
@@ -0,0 +1,36 @@
+package com.huuloc.bookstore.bbook.config;
+
+import com.lib.payos.PayOS;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class PayOSConfig implements WebMvcConfigurer {
+    @Value("${payos.client-id}")
+    private String clientId;
+
+    @Value("${payos.api-key}")
+    private String apiKey;
+
+    @Value("${payos.checksum-key}")
+    private String checksumKey;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600); // Max age of the CORS pre-flight request
+    }
+
+    @Bean
+    public PayOS payOS() {
+        return new PayOS(clientId, apiKey, checksumKey);
+    }
+}
